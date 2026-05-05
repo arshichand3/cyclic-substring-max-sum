99d145e97e4462f808d4038f0eaf5f57e6101041
arshichand3 <arshichand3@gmail.com>
2026-05-05 17:51:30 +0530

    Added Problem 1 solution

--- Problem1.java
+++ Problem1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Solution {
+public class Problem1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
