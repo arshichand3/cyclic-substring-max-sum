b9ff67081a3aa5857b8f756adda394da65a5ca0e
arshichand3 <arshichand3@gmail.com>
2026-05-05 17:55:00 +0530

    Added problem 2

--- problem2.java
+++ problem2.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Problem2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+ 
         int n = sc.nextInt();
         int[] arr = new int[n];
 
